0cab55faf85df12354f8c926f1d96c7b40e2ff10
Garikoitz Belarbide <gbelarbide@email.com>
2026-01-15 17:56:12 +0100

    feat: add Get-GbHelp function to display available module functions and their descriptions.

--- gbtools.psm1
+++ gbtools.psm1
@@ -4,7 +4,7 @@
 
 
 .DESCRIPTION
-    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/gbelarbide/SC-online/refs/heads/main/gbtools.psm1') | Invoke-Expression ; Get-HolaMundo
+    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/gbelarbide/SC-online/refs/heads/main/gbtools.psm1') | Invoke-Expression ; Get-GbHelp
     (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/gbelarbide/SC-online/refs/heads/main/Tools/desinstala.psm1') | Invoke-Expression ; Start-GbDesintala
    
 
@@ -16,6 +16,38 @@
 #>
 
 #region [Functions]-------------------------------------------------------------
+Function Get-GbHelp {
+    <#
+    .SYNOPSIS
+        Muestra las funciones disponibles en el módulo gbtools
+    
+    .DESCRIPTION
+        Muestra una lista de todas las funciones disponibles en el módulo gbtools con una breve descripción de cada una
+    
+    .EXAMPLE
+        Get-GbHelp
+    #>
+    
+    Write-Host "`n=== Funciones Disponibles en gbtools ===" -ForegroundColor Cyan
+    Write-Host ""
+    
+    $functions = @(
+        [PSCustomObject]@{
+            Función     = "Get-GbHelp"
+            Descripción = "Muestra esta ayuda con las funciones disponibles"
+        },
+        [PSCustomObject]@{
+            Función     = "Start-GbDesintala"
+            Descripción = "Descarga y ejecuta el módulo de desinstalación de aplicaciones"
+        }
+    )
+    
+    $functions | Format-Table -AutoSize -Wrap
+    
+    Write-Host "Para más información sobre una función específica, usa: Get-Help <Nombre-Función> -Detailed" -ForegroundColor Yellow
+    Write-Host ""
+}
+
 
 Function Start-GbDesintala {
     (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/gbelarbide/SC-online/refs/heads/main/Tools/desinstala.psm1') | Invoke-Expression
